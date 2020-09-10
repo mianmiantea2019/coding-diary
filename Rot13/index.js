d302bb77185cc4c1a04ef2fcf95dc5d08fe147c4
Christy <mianmianguo2020@gmail.com>
2020-09-10 16:17:56 -0400

    upload Rot13 with better solution

--- Rot13/index.js
+++ Rot13/index.js
@@ -1,5 +1,34 @@
-function rot13(message){
+
+  let message;
+  
+  // function rot13(message){
+  //   let letterArray="";
+
+  //   //your code here
+  //  const listA ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  //  const listB = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+  //  const newListA = listA.split("")
+  //  const newListB = listB.split("")
+  //  const newmessage = message.split("")
+
+  //  for (let i = 0; i < newmessage.length; i ++) {
+  //   let newLetterIndex = newListA.indexOf(message[i]);
+  //   letterArray += newListB[newLetterIndex]
+  //  }
+  //  console.log(   letterArray.toString()
+  //  )
+  // }
+
+
+  function rot13(message){
+
     //your code here
+   const a ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+   const b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+   return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
   }
 
-  
+
+  rot13('message')
